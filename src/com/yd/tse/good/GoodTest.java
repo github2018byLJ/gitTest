224d1618e2a8dd849d07af6ebbe584ae0610e97e
Y01024 <lijian@yondervision.com.cn>
2018-11-06 09:28:45 +0800

    hot_fix修复bug,打印日志

--- src/com/yd/tse/good/GoodTest.java
+++ src/com/yd/tse/good/GoodTest.java
@@ -3,7 +3,7 @@ package com.yd.tse.good;
 public class GoodTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hot_fix change GoodTest");
 
 	}
 
